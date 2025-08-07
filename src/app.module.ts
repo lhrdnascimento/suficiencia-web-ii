d14e91c91c3da583165583307ea3443ec28ba911
Lucas Hoffmann <lhrdnascimento@furb.br>
2025-08-07 16:58:54 -0300

    chore: create bill module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BillModule } from './bill/bill.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BillModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
